462ca87fb18f4908c7663c8ee3ed891f23d06e16
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-09-24 14:29:07 -0400

    ran first test contract deploys

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -18,3 +18,10 @@ beforeEach(async () => {
     .deploy({data: bytecode})
     .send({ from: accounts[0], gas:'1000000'});
 });
+
+
+describe ('Lottery Contract', ()=>{
+it('deploys a contract',() => {
+assert.ok(lottery.options.address);
+});
+});
